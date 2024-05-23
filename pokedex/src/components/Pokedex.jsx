73bcc64a271909880d9c9f0e53e205ecd67829b9
Arthur.c.s <130755987+arthur-canofre@users.noreply.github.com>
2024-05-23 12:25:50 -0300

    commit inicial

---
+++ pokedex/src/components/Pokedex.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react"
+
+export default function Pokedex() {
+
+    const [id , setId] = useState(1)
+    const [pokemon, setPokemon] = useState(null)
+
+    const fecthData = async () => {
+        try{
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const data = await response.json()
+            setPokemon(data)
+        }catch(error){
+            console.error("Error: ", error)
+        }
+    }
+
+    const nextPokemon = () =>(
+        setId(id + 1)
+    )
+
+    const previousPokemon = () =>(
+        setId(id - 1)
+    )
+
+    useEffect(() =>{
+        fecthData()
+    }, [id])    
+
+    return(
+
+        <div>
+            { pokemon && (
+                <div className="pokemon">
+                    <h1>Pokemon</h1>
+                    <p>{pokemon.id}</p>
+                    <p>{pokemon.name}</p>
+                    <p>Peso: {pokemon.weight}</p>
+                    <img src={pokemon.sprites.front_default} alt="" />
+
+                    <button onClick={previousPokemon}>Anterior</button>
+                    <button onClick={nextPokemon}>Próximo</button>
+                </div>
+            )
+
+            }
+        </div>
+       
+    )
+}

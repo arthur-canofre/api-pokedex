50c51070190d79ae9a7a82cd0d39b675c653c7b6
Arthur.c.s <130755987+arthur-canofre@users.noreply.github.com>
2024-06-03 12:22:15 -0300

    commit carregamento

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,15 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import style from 'styled-components'
+import { motion } from "framer-motion"
 import Pokedex from './components/Pokedex'
 
+const Spinner = style(motion.div)`
+width: 50px;
+height: 50px;
+border: 4px solid black;
+border-top: 4px solid #fff;
+border-radius: 50%;
+display: inline-block;
+`;
+
+const Container = style.div`
+display: flex;
+width: 100w;
+align-items: center;
+justify-content: center;
+height: 100vh;
+`;
+
 function App() {
-  return (
-    <>
-    <Pokedex/>
-    </>
-  )
+    const [carregando, setCarregando] = useState(true);
+
+useEffect(() => {
+  const tempo = setTimeout(() => {
+    setCarregando(false);
+  }, 5000)
+  return () => clearTimeout(tempo)
+}, [])
+
+return (
+  <Container>
+    {carregando ?(
+    <Spinner
+      animate={{ rotate: 360 }}
+      transition={{ duration: 1, repeat: Infinity, ease: 'linear' }}>
+
+    </Spinner>
+    ):(
+      <Pokedex/>
+    )}
+  </Container>
+
+)
 }
 
 export default App
